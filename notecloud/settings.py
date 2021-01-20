21d1eba6eed66d89db4df6f3b6adcbe6a94b3cd8
KJ Kim <gencode.me@gmail.com>
2021-01-20 13:02:10 +0900

    cors setting

--- notecloud/settings.py
+++ notecloud/settings.py
@@ -110,7 +110,7 @@ ALLOWED_HOSTS = ['*']
 # CORS headers
 # https://github.com/adamchainz/django-cors-headers
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8080"
