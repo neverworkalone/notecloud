5dfedab3a2770c959ff1b721d75196b6636ec810
KJ Kim <gencode.me@gmail.com>
2021-03-05 13:12:44 +0900

    Update test_bot.py

--- tests/utils/test_bot.py
+++ tests/utils/test_bot.py
@@ -1,29 +1,10 @@
-import accounts
-
-from django.utils import timezone
-
 from core.response import Response
 from core.testcase import TestCase
 
 
 class BotTest(TestCase):
     def setUp(self):
-        yesterday = timezone.now() - timezone.timedelta(1)
-        self.expired_user = accounts.models.User.objects.create_user(
-            username='8@a.com',
-            password='12345678',
-            is_approved=True,
-            is_prime=True,
-            prime_until=yesterday.date()
-        )
-        self.prime_user = accounts.models.User.objects.create_user(
-            username='88@a.com',
-            password='12345678',
-            is_approved=True,
-            is_prime=True,
-            prime_until=timezone.now().date()
-        )
-        self.create_user(is_staff=True)
+        self.create_user(is_prime=True, is_staff=True)
         self.create_memo()
 
     def test_daily_check_prime_user(self):
